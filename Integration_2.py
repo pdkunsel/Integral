e51d5cb6184355dbb895408204bcf3589dbff5a4
Peyton <pdkunsel@buffalo.edu>
2021-02-18 10:41:16 -0500

    Adds the file Integration_2.py which is another file that uses Simpson's Rule to calculate an integral of a quadratic function.

---
+++ Integration_2.py
@@ -0,0 +1,23 @@
+'''
+The purpose of this program is to numerically calculate an integral using Simpson's Rule for a given function
+'''
+import my_library
+
+
+def f(x):            # The variable for the function you wish to integrate
+    return 4-x**3
+
+def integral(f, a, b, n):
+    dx = (b-a)/float(n)
+    sum1 = 0
+    for i in range(1, n/2 +1):     #Computes the partial sum for the odd components
+        sum1 += f(a + (2*i - 1)*dx)
+    sum1 *= 4
+    sum2 = 0 
+    for i in range (1,n/2):            #Computes the partial sum for the even components
+        sum2 += f(a + 2*i*dx)
+    sum2 *= 2
+    approx = dx/3.0 * (f(a)+f(b)+sum1+sum2)
+    return approx
+
+print(integral(f, 0, 1, 1000))
